b18a281be59be6e707013198f9b196ea86a47294
Lauri <lauri.vuorela@gmail.com>
2017-10-26 10:52:15 +0200

    listening for more events and new mail. removed exclamation mark in slack notification due to adding new mail. fixed severeal events showing correctly if happening at the same time in slack notifications

--- svm-notifications.py
+++ svm-notifications.py
@@ -8,23 +8,25 @@ from bs4 import BeautifulSoup
 
 loop = asyncio.get_event_loop()
 sem = asyncio.Semaphore(5)
-session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(
-        verify_ssl=False))
-with open(os.path.join(os.path.dirname(__file__), 'data_svm.json'),
-          'r', encoding='utf-8') as json_file:
+session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False))
+with open(os.path.join(os.path.dirname(__file__), 'data_svm.json'), 'r', encoding='utf-8') as json_file:
     json_data = json.loads(json_file.read())
     svm_usr = json_data.get('svm_usr')
     svm_pwd = json_data.get('svm_pwd')
     webhook = json_data.get('slack_webhook')
     payload_text = json_data.get('slack_payload')
+    original_text = payload_text['text']
 report = open(os.path.join(os.path.dirname(__file__), 'report_svm.txt'), 'a+')
+keywords = ['biz', 'betalt']
 
 
 async def post_slack(event):
     global payload_text
+    global original_text
     global webhook
-    payload_text['text'] = '{}{}!'.format(payload_text['text'], event)
+    payload_text['text'] = '{}{}'.format(original_text, event)
     payload = json.dumps(payload_text)
+
     try:
         async with sem, session.post(webhook, data=payload) as response:
             return await response.read()
@@ -60,12 +62,22 @@ try:
     nyamess = the_page.find(id='nyamess')
     events = nyamess.descendants
     for event in events:
-        if ('biz' in str(event.string)
-                and 'NavigableString' in str(event.__class__)):
+        for keyword in keywords:
+            if (keyword in str(event.string) and 'NavigableString' in str(event.__class__)):
+                hits += 1
+                loop.run_until_complete(post_slack(event.string))
+except Exception:
+    pass
+try:
+    new_mail = the_page.find(id='new_mail')
+    events = new_mail.descendants
+    for event in events:
+        if ('mail!' in str(event.string) and 'NavigableString' in str(event.__class__)):
             hits += 1
             loop.run_until_complete(post_slack(event.string))
 except Exception:
     pass
+
 if hits > 0:
     report.write('{}: new biz!\n'.format(now))
 else:
